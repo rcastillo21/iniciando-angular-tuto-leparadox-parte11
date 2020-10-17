3eed685e2b1a884a8800aebbdd41c466f72457df
MSCINSYSTEMS-PC\MSC IN SYSTEMS <Ruben>
2020-10-17 08:01:34 -0430

    Se crearon Componentes Basicos y Componentes para los Subject, se realizo configuración basica.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { SubjectComponent } from './components/subject/subject.component';
+import { ReplaySubjectComponent } from './components/replay-subject/replay-subject.component';
+import { BehaviorSubjectComponent } from './components/behavior-subject/behavior-subject.component';
+import { AsyncSubjectComponent } from './components/async-subject/async-subject.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubjectComponent,
+    ReplaySubjectComponent,
+    BehaviorSubjectComponent,
+    AsyncSubjectComponent
   ],
   imports: [
     BrowserModule
